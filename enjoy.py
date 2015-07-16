3dcef7df1fff983520ebe27f795f030925d5a379
Mattia Dimauro <mattiadmr@gmail.com>
2015-07-16 18:33:24 +0200

    first commit

---
+++ enjoy.py
@@ -0,0 +1,64 @@
+import json, requests, pymongo, time
+
+class Enjoy:
+
+    def __init__(self):
+
+        self.endpoint = 'https://enjoy.eni.com'
+
+        self.api = {
+            'vehicles': self.endpoint+'/get_vetture'
+            }
+
+        self.sleeptime = 0.5
+
+        connection = pymongo.Connection()
+        self.db = connection['enjoy']
+        self.vehicles = self.db['vehicles']
+        self.shifts = self.db['shifts']
+
+
+    def get_vehicles(self):
+        response = None
+        backoff = 20
+        while response == None:
+            try:
+                response = json.loads(requests.get(api['vehicles']).text)
+            except Exception:
+                print "Somenthing went wrong with the requests, retrying in {0} seconds.".format(backoff)
+                time.sleep(backoff)
+                backoff *= 1.2
+                continue
+
+        return response
+
+    def update(self):
+        vehicles_data = get_vehicles()
+
+        for _vehicle in vehicles_data:
+            vehicle = {
+                '_id': _vehicle['plate'],
+                'lat': _vehicle['lat'],
+                'lon': _vehicle['lon'],
+                'time': datetime.datetime.now()
+            }
+
+            self.vehicles.save(vehicle)
+            _vehicle = self.vehicles.find({'_id': _vehicle['plate']})
+
+            if vehicle['lat'] != _vehicle['lat'] && vehicle['lon'] != _vehicle['lon']:
+                print "shift!"
+                shift = {
+                    'plate': vehicle['plate'],
+                    'a_lat': _vechile['lat'],
+                    'a_lon': _vehicle['lon'],
+                    'b_lat': vehicle['lat'],
+                    'b_lon': vehicle['lon']
+                }
+
+                self.shifts.insert(shift)
+
+    def observe(self):
+        while(True):
+            self.update()
+            time.sleep(5)
