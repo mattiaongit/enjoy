ffc932c6ad2974f57804a12c7cee568192be9c4f
Mattia Dimauro <mattiadmr@gmail.com>
2015-07-17 10:58:46 +0200

    debugging

--- enjoy.py
+++ enjoy.py
@@ -46,6 +46,9 @@ class Enjoy:
             self.vehicles.save(vehicle)
             _vehicle = self.vehicles.find({'_id': _vehicle['car_plate']})
 
+            print vehicle['lat']
+            print _vehicle['lat']
+
             if vehicle['lat'] != _vehicle['lat'] and vehicle['lon'] != _vehicle['lon']:
                 print "shift!"
                 shift = {
